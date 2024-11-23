27a498be5051f63c7342052dbd2ae07b5d8498ae
Waduzzaman <waduzzaman@gmail.com>
2024-11-22 22:37:03 -0500

    qs updated

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
+  eslint:{
+    ignoreDuringBuilds:true
+}
+  
+  
   /* config options here */
 };
 
